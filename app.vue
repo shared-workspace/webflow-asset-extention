b542d6ce852a7f9e829c18f23583b61aa510d7ba
m1n199honey <90564711+m1n199honey@users.noreply.github.com>
2024-10-07 08:00:01 +0000

    data client + designer setup

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+  </div>
+  <NuxtPage />
+</template>
--- backend/server/api/authorize.ts
+++
@@ -1,9 +0,0 @@
-import { WebflowClient } from "webflow-api";
-
-export default defineEventHandler((event) => { // Cannot find name 'defineEventHandler'.
-  const authorizeUrl = WebflowClient.authorizeURL({
-    scope: ["sites:read", "authorized_user:read"],
-    clientId: process.env.CLIENT_ID!,
-  });
-  sendRedirect(event, authorizeUrl); // Cannot find name 'sendRedirect'.
-});
